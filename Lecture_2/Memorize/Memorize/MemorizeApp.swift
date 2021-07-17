9a7df834b8e56819705a419e4cc883932f44c4cd
BGoon987 <BGoon0128@protonmail.ch>
2021-07-17 23:39:47 +0900

    210717 initial commit

---
+++ Lecture_2/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 裴良洙 on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
